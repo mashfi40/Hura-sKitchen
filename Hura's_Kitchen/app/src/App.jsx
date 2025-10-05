a01f443e5ffd99d56b663a331fbd5ab8278f474a
mashfi40 <mashfi1907040@stud.kuet.ac.bd>
2025-10-05 22:17:27 +0600

    first commit

---
+++ Hura's_Kitchen/app/src/App.jsx
@@ -0,0 +1,162 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import SearchResult from "./components/SearchResults/SearchResult";
+
+export const BASE_URL = "http://localhost:9000";
+
+
+
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedBtn, setSelectedBtn] = useState("all");
+
+  
+ useEffect (() => {
+  const fetchFoodData = async () => {
+    setLoading(true);
+    
+    try{
+      const response = await fetch(BASE_URL);
+      const json = await response.json();
+
+      setData(json); 
+      setFilteredData(json);
+      setLoading(false);
+    }
+    catch (error) {
+      setError("Unable to fetch data");
+    }
+  };
+  fetchFoodData();
+ }, []);
+
+ const searchFood = (e) => {
+  const searchValue = e.target.value;
+  console.log(searchValue);
+
+  if(searchValue == ""){
+    setFilteredData(null);
+  }
+  const filter = data?.filter((food) => food.name.toLowerCase().includes(searchValue.toLowerCase()));
+  setFilteredData(filter);
+ };
+
+ const filterFood = (type) => {
+  if(type == "all") {
+    setFilteredData(data);
+    setSelectedBtn("all");
+    return;
+  }
+  const filter = data?.filter((food) => 
+    food.type.toLowerCase().includes(type.toLowerCase()));
+  setFilteredData(filter);
+  setSelectedBtn(type);
+ }
+
+ const filterBtns = [
+  {
+    name: "All",
+    type: "all",
+  },
+  {
+    name: "Appetizer",
+    type: "appetizer",
+  },
+  {
+    name: "Main",
+    type: "main",
+  },
+  {
+    name: "Dessert",
+    type: "dessert",
+  },
+  {
+    name: "Drink",
+    type: "drink",
+  },
+ ];
+
+  if(error) return <div>{error}</div>;
+  if(loading) return <div>loading...</div>;
+
+  return (
+  <>
+  <Container>
+    <TopContainer>
+      <div className="logo">
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      <div className="search">
+        <input onChange={searchFood} placeholder="Search Menu"/>
+      </div>
+    </TopContainer>
+
+    <FilterContainer>
+      {
+        filterBtns.map((value) => (
+          <Button 
+          isSelected={selectedBtn==value.type}
+          key={value.name} onClick={() => filterFood(value.type)}>{value.name} </Button>
+        ))
+      }
+    </FilterContainer>
+  </Container>
+  <SearchResult data = { filteredData }/>
+  </>
+  );
+}
+
+export default App;
+
+export const Container = styled.div`
+   max-width: 1200px;
+   margin: 0 auto;
+`;
+const TopContainer = styled.section`
+   height: 140px;
+   display: flex;
+   justify-content: space-between;
+   padding: 16px;
+   align-items: center;
+
+   .search {
+     input {
+       background-color: transparent;
+       border: 1px solid red;
+       color: white;
+       border-radius: 5px;
+       height: 40px;
+       font-size: 16px;
+       padding: 0 10px; 
+       &::placeholder{
+        color: white;
+       }
+     }
+   }
+   @media (0 < width < 600px) {
+      flex-direction: column;
+      height: 120px;
+   }
+`;
+const FilterContainer = styled.section`
+   display: flex;
+   justify-content: center;
+   gap: 12px;
+   padding-bottom: 40px;
+`;
+export const Button = styled.button`
+   background: ${({isSelected}) => (isSelected? "#ba2121ff": "#ff4343")};
+   outline: 1px solid ${({isSelected}) => (isSelected? "white": "#ff4343")};
+   border-radius: 5px;
+   padding: 6px 12px;
+   border: none;
+   color: white;
+   cursor: pointer;
+   &:hover{
+     background-color: #ba2121ff; 
+   } 
+`;
---
+++ Hura's_Kitchen/server/src/index.ts
@@ -0,0 +1,120 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
+
+app.get("/", (req, res) => {
+  console.log(path.join(__dirname, "../public"));
+  const foodData = [
+    {
+        name: "Dalpuri",
+        price: 270,
+        text: "মশলাদার ডাল ও চালের মিশ্রণ, গরম তেলে ভাজা, সান্ধ্য খাবারে আদর্শ।",
+        image: "/images/dalpuri.png",
+        type: "appetizer",
+        },
+        {
+        name: "Fuchka",
+        price: 150,
+        text: "টাটকা মশলা, পেঁয়াজ ও ছোলা দিয়ে ভর্তি, মজাদার পানীয় সহ খেতে সুস্বাদু।",
+        image: "/images/Fuchka.png",
+        type: "appetizer",
+        },
+        {
+        name: "Macher Chop",
+        price: 350,
+        text: "মুচমুচে ভাজা, সুস্বাদু মাছের মাংস ও মশলা দিয়ে তৈরি টেস্টি চপ।",
+        image: "/images/macherchop.png",
+        type: "appetizer",
+        },
+        {
+        name: "Masala Crostini",
+        price: 450,
+        text: "মসলাযুক্ত টোস্ট, সুগন্ধি মশলা ও চিজ দিয়ে সাজানো, একটি ফিউশন স্ন্যাকস।",
+        image: "/images/MasalaCrostini.png",
+        type: "appetizer",
+        },
+        {
+        name: "Masala Dim",
+        price: 250,
+        text: "মশলার সাথে সেদ্ধ ডিম, স্বাদে ভরপুর এবং সহজে তৈরি করা যায়।",
+        image: "/images/masaladim.png",
+        type: "lunch",
+        },
+        {
+            name: "Achari Mangsho",
+            price: 380,
+            text: "মাংসের টুকরো মশলা ও আচার দিয়ে রান্না, তেতো-মিষ্টি স্বাদে পরিপূর্ণ।",
+            image: "/images/acharimangsho.png",
+            type: "main",
+        },
+        {
+            name: "Kolapata Vaat",
+            price: 420,
+            text: "সুগন্ধি গরম ভাত, কলাপাতা দিয়ে সেঁকা, গ্রাম্য ঐতিহ্যের একটি দারুণ খাবার।",
+            image: "/images/kolapataVaat.png",
+            type: "main",
+        },
+        {
+            name: "Panta Ilish",
+            price: 640,
+            text: "পান্তা ভাতের সাথে তাজা ইলিশ মাছ, ভাপা মশলা সহ, মৌসুমি স্বাদ।",
+            image: "/images/pantaIlish.png",
+            type: "main",
+        },
+        {
+            name: "Shorisha Ilish",
+            price: 700,
+            text: "সরিষা সসের সাথে রান্না করা ইলিশ মাছ, মিষ্টি ও তিক্ত স্বাদের মিশ্রণ।",
+            image: "/images/shorishaIlish.png",
+            type: "main",
+        },
+        {
+        name: "Mishti Doi",
+        price: 300,
+        text: "সঠিকভাবে মিষ্টি ও ক্রিমি দই, বাঙালি মিষ্টির আদর্শ প্রতীক।",
+        image: "/images/mistidoi.png",
+        type: "dessert",
+        },
+        {
+        name: "Payesh",
+        price: 380,
+        text: "দুধ, চিনি ও চালের মিষ্টি রেসিপি, বাঙালির পছন্দের পিঠা ও মিষ্টি।",
+        image: "/images/payesh.png",
+        type: "dessert",
+        },
+        {
+        name: "Aam Panna",
+        price: 230,
+        text: "কাঁচা আমের সাৎ, মিষ্টি ও টক, গরমে মন মাতানো ঠাণ্ডা পানীয়।",
+        image: "/images/aampanna.png",
+        type: "drink",
+        },
+        {
+        name: "Dudh Cha",
+        price: 70,
+        text: "হালকা মিষ্টি, চা পাতা ও দুধের সুমধুর মিশ্রণ, সব সময় প্রিয় পানীয়।",
+        image: "/images/cha.png",
+        type: "drink",
+        },
+        {
+        name: "Malai Cha",
+        price: 100,
+        text: "দুধ ও মসলা চায়ের সাথে রিচ মালাইয়ের মিশ্রণ, বিশেষ স্বাদে এক কাপ শান্তি।",
+        image: "/images/malaicha.png",
+        type: "drink",
+        },
+  ];
+
+  res.json(foodData);
+});
+
+app.listen(9000, () => {
+  console.log("Server is running on port 9000");
+});
